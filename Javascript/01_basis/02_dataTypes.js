8291407dfcb93f24d32fc862f3ef65c496851ea4
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-16 00:23:31 +0530

    Initial commit of data types demonstration in JavaScript

---
+++ Javascript/01_basis/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello");  // we are using node.js not browser
+
+console.log(3 + 3);
+console.log(4 + 4);
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = true;
+
+// Primitive data types
+// number - Range: -(2^53 - 1) to 2^53 - 1 (i.e., -9007199254740991 to 9007199254740991)
+// bigInt - Range: Larger than Number.MAX_SAFE_INTEGER (i.e., beyond -9007199254740991 to 9007199254740991), limited only by available memory
+// String
+// Boolean - true, false
+// null - standalone value representing "empty" or "no value"
+// undefined
+// Symbol - unique
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
