189ac232d341c23352d1fa7739040c0a5a9607ad
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-16 00:58:22 +0530

    Add conversion operations and type demonstrations in JavaScript

---
+++ Javascript/01_basis/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = undefined;
+
+// console.log(typeof score);
+
+let valueInNumner = Number(score);
+
+// console.log(typeof valueInNumner);
+// console.log(valueInNumner);
+// console.log(typeof NaN);
+
+// "33" = 33
+// "33abc" = NaN typeof NaN = number
+// true = 1
+// false = 0
+// undefined = NaN
+
+let isLoggedIn = "hitseh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 = true
+// 0 = false
+// "" = false
+// "hitesh" = true
+// -2 = true
+
+let someNumber = -33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
