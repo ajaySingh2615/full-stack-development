c8364ae94d3e1bedb4069f0dfdc59da94fec1156
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-17 18:21:37 +0530

    Add course object and demonstrate object destructuring in JavaScript

---
+++ Javascript/03_objects/03_de-structure.js
@@ -0,0 +1,11 @@
+const course = {
+  courseName: "js in hindi",
+  price: 999,
+  courseInstructor: "hitesh",
+};
+
+// course.courseInstructor
+
+const { courseInstructor: instructor } = course;
+
+console.log(instructor);
