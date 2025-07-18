afafc47c1eed65fdd03adee4061860f25c9c5cee
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-18 11:37:39 +0530

    Add calculateCartPrice function and user object with logging functionality in JavaScript

---
+++ Javascript/04_functions/02_functions.js
@@ -0,0 +1,18 @@
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(2, 400, 500));
+
+const user = {
+  username: "hitesh",
+  price: 199,
+};
+
+function handleObject(anyObject) {
+  console.log(
+    `user name is ${anyObject.username} and price is ${anyObject.price}`
+  );
+}
+
+handleObject(user);
