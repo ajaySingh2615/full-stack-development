6af8bd9573a0794aa3bf0fa0427f5f074c39a857
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-20 23:40:18 +0530

    Add database connection setup and constants for MongoDB integration in the backend

--- Backend/myTube/src/index.js
+++ Backend/myTube/src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
 
 const PORT = process.env.PORT || 8001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error in connecting to MongoDB", err);
+    process.exit(1);
+  });
