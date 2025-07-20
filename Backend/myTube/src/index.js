2a586f731449e8350539a652ca75b439a032247c
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-20 23:14:18 +0530

    Add CORS and dotenv middleware to Express app; update package.json and package-lock.json for new dependencies

--- Backend/myTube/src/index.js
+++ Backend/myTube/src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT || 8001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
