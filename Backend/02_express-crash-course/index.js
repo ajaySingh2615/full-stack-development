d392f56368c05e55fb6c2aadaff74bab8c0cc0bc
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-20 19:01:40 +0530

    Add examples of some common array methods: concat, slice, and splice in JavaScript

---
+++ Backend/02_express-crash-course/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+let teaData = [];
+let nextId = 1;
+
+// Create a new tea
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = { id: nextId++, name, price };
+  teaData.push(newTea);
+  res.status(201).send(newTea);
+});
+
+// Get all teas
+app.get("/teas", (req, res) => {
+  res.status(200).send(teaData);
+});
+
+// Get a specific tea
+app.get("teas/:id", (req, res) => {
+  const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
+  if (!tea) {
+    res.status(404).send("Tea not found");
+  }
+  res.status(200).send(tea);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
