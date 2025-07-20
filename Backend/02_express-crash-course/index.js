af27b878c30b172a424c6560dcec74cbbb960c95
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-20 19:22:48 +0530

    Add CRUD operations for tea management in the Express app, including GET, PUT, and DELETE endpoints.

--- Backend/02_express-crash-course/index.js
+++ Backend/02_express-crash-course/index.js
@@ -21,12 +21,36 @@ app.get("/teas", (req, res) => {
 });
 
 // Get a specific tea
-app.get("teas/:id", (req, res) => {
+app.get("/teas/:id", (req, res) => {
   const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
+  console.log(tea);
   if (!tea) {
     res.status(404).send("Tea not found");
   }
   res.status(200).send(tea);
+});
+
+// Update a tea
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
+  if (!tea) {
+    res.status(404).send("Tea not found");
+  }
+
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.status(200).send(tea);
+});
+
+// Delete a tea
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((tea) => tea.id === parseInt(req.params.id));
+  if (index === -1) {
+    res.status(404).send("Tea not found");
+  }
+  teaData.splice(index, 1);
+  res.status(204).send();
 });
 
 app.listen(port, () => {
