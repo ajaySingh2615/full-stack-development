d432e1059626df9f3d1fa12abc58e58a3e0e7d44
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-21 19:00:49 +0530

    Implement logout functionality in user controller; update user routes to include secure logout endpoint with JWT verification, enhancing session management and security.

--- Backend/myTube/src/controllers/user.controllers.js
+++ Backend/myTube/src/controllers/user.controllers.js
@@ -160,7 +160,26 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  // need to come back here afer middlewares video
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    { new: true }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
