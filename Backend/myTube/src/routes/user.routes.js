008d9ecf6920b30b7aa7e66f179e2e694e73eb66
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-26 21:01:06 +0530

    Add refresh token endpoint to user routes; enhance session management by allowing users to obtain new access tokens, improving overall authentication flow.

--- Backend/myTube/src/routes/user.routes.js
+++ Backend/myTube/src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   registerUser,
   logoutUser,
   loginUser,
+  refreshAccessToken,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
@@ -22,5 +23,6 @@ router.route("/register").post(
 // secure routes
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
